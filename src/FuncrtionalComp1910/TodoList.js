504dc0acba42fefe4e4f5d7c78450e3445aca365
Sandeep G <dev.sandeep@hotmail.com>
2023-01-09 22:09:48 +0530

    Update TodoList.js

--- src/FuncrtionalComp1910/TodoList.js
+++ src/FuncrtionalComp1910/TodoList.js
@@ -1,35 +1,35 @@
-import React from "react";
+import * as React from 'react';
+import Table from 'react-bootstrap/Table';
 
-const TodoList = (props)=>{
-    const deleteHandler = (id)=>{
-        console.log("delete", id);
-        props.callback(id);
-    }
+const TodoList = (props) => {
 
-    const editHandler = (id, existingText)=>{
-        const newText = prompt("Enter the updated text", existingText);
-        console.log(newText);
-    }
     return (
-        <>
-            {/* {props.list.map(()=>{
-                return <></>
-            })} */}
-            {props.list.map((el)=>(<tr>
-                <td>{el.id}</td>
-                <td>{el.text}</td>
-                <td>
-                    <button onClick={()=>{
-                        deleteHandler(el.id)
-                    }}>Delete</button>
-                    <button onClick={()=>{
-                        editHandler(el.id, el.text)
-                    }}>Edit</button>
-                </td>
-            </tr>))}
-            
+        <section id="todo-list">
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Task Name</th>
+                        <th colSpan={2}>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.tasks.map((el, key) => {
+                        return <><tr>
+                            <td>{key + 1}</td>
+                            <td>{el.name}</td>
+                            <td><button onClick={()=>props.onEdit(el.id, el.name)}>Edit</button></td>
+                            <td><button onClick={()=>props.onDelete(el.id)}>Delete</button></td>
+                        </tr>
+                        </>
+                    })}
 
-        </>
+                </tbody>
+            </Table>
+        </section>
     );
 }
+
+
+
 export default TodoList;
